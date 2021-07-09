d89dbb8c5c0eca2b2d7bbda2af1a722cefb115c8
Memo Chou <memochou1993@gmail.com>
2021-07-09 20:44:23 +0800

    Add a form

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,17 +1,51 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
+  <div
+    class="row items-center justify-center q-mt-xl"
+  >
+    <div
+      class="col-xs-10 col-sm-8"
     >
-  </q-page>
+      <q-card>
+        <q-card-section>
+          <q-form
+            class="q-gutter-md"
+          >
+            <q-input
+              v-model="source"
+              input-class="text-body1 text-primary"
+              outlined
+              placeholder="URL"
+            >
+              <template
+                #after
+              >
+                <q-btn
+                  color="primary"
+                  label="Shorten"
+                  size="lg"
+                />
+              </template>
+            </q-input>
+          </q-form>
+        </q-card-section>
+      </q-card>
+    </div>
+  </div>
 </template>
 
 <script>
-import { defineComponent } from 'vue';
+import {
+  defineComponent,
+  ref,
+} from 'vue';
 
 export default defineComponent({
   name: 'PageIndex',
+  setup() {
+    const source = ref('');
+    return {
+      source,
+    };
+  },
 });
 </script>
